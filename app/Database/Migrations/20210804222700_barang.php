710fcd331a0b9c3c6ab4c0293882faadfbec7a4f
insert github real name here <insert github email here>
2021-08-05 09:09:52 +0800

    first commit

---
+++ app/Database/Migrations/20210804222700_barang.php
@@ -0,0 +1,55 @@
+<?php namespace App\Database\Migrations;
+
+class Barang extends \CodeIgniter\Database\Migration{
+
+
+	public function up(){
+		$this->forge->addField([
+			'id'=>[
+				'type'=>'INT',
+				'constraint'=>11,
+				'unsigned'=>TRUE,
+				'auto_increment'=>TRUE
+			],
+			'nama'=>[
+				'type'=>'TEXT',
+			],
+			'harga'=>[
+				'type'=>'INT',
+				'constraint'=>11,
+			],
+			'stok'=>[
+				'type'=>'INT',
+				'constraint'=>11,
+			],
+			'gambar'=>[
+				'type'=>'TEXT'
+			],
+			'created_by'=>[
+				'type'=>'INT',
+				'constraint'=>11,
+			],
+			'created_date'=>[
+				'type'=>'DATETIME',
+			],
+			'updated_by'=>[
+				'type'=>'INT',
+				'constraint'=>11,
+				'null'=>TRUE,
+			],
+			'updated_date'=>[
+				'type'=>'DATETIME',
+				'null'=>TRUE,
+			]
+		]);
+
+		$this->forge->addKey('id',TRUE);
+		$this->forge->createTable('Barang');
+	}
+
+	public function down(){ 
+		$this->forge->dropTable('Barang');
+
+	}
+}
+?>
---
+++ app/Views/hello/world.php
@@ -0,0 +1,5 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?>
+<h1>Welcome to Mabalanca </h1>
+<?= $data ?>
+<?= $this->endSection() ?>
